e8ac0c37786dd0afa591160e2491d0e17c463c45
keisuke03 <92214736+keisuke03@users.noreply.github.com>
2023-07-15 11:09:32 +0900

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,6 +1,6 @@
 import os
-token = "ここにdiscordトークンを入力"#@param {type:"string"}
+token = "MTEyNjA0NTIwMDQ5NDYzMjk4MQ.G454eE.l-aEtT3QCAl9RazFR9UdV-0v0NAUb_Yy0zW8is"#@param {type:"string"}
 os.environ['TOKEN'] = token
 
-openai_apikey = "ここにOpenAIのAPI Keyを入力"#@param {type:"string"}
+openai_apikey = "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"#@param {type:"string"}
 os.environ['OPENAI_API'] = openai_apikey
